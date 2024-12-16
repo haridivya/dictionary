8354110d71d14c7f06e5c8e954b36dc1d6ae1a2b
haridivya <123063502+haridivya@users.noreply.github.com>
2024-12-16 11:40:45 +0530

    Update sum_all_value.py

--- sum_all_value.py
+++ sum_all_value.py
@@ -13,3 +13,11 @@ Sample Input:
 Sample Output :
 879
 '''
+n=int(input())
+sum_values=0
+di={}
+for i in range(n):
+    di[i]=int(input())
+for j in di.values():
+    sum_values+=j
+print(sum_values)
